353756759e39f7c592f58941ccad0ffe41e23139
Okashe <okashabdi88@gmail.com>
2023-05-28 12:36:37 -0500

    image centered

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,19 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
-          title: const Center(
-            child: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[900],
+            title: const Center(
+              child: Text('I Am Rich'),
+            ),
           ),
-        ),
-        body: const Image(
-          image: NetworkImage(
-              'https://miro.medium.com/v2/resize:fit:3840/1*xMuIOwjliGUPjkzukeWKfw.jpeg'),
-        ),
-      ),
+          body: const Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://miro.medium.com/v2/resize:fit:3840/1*xMuIOwjliGUPjkzukeWKfw.jpeg'),
+            ),
+          )),
     ),
   );
 }
